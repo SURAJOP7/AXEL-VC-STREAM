1ef6b194460baea19dd830cb9c6984c53feaa6d8
SURAJ-xD <85247626+SURAJOP7@users.noreply.github.com>
2021-09-17 09:20:38 +0530

    Update start.py

--- player/start.py
+++ player/start.py
@@ -4,11 +4,11 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Hey, I'm a VC Video Player developed by Developers of** @SURAJ_O_P \n\n**To use it:-** __ \n1) Add this Bot to your Group and Make it Admin \n2) Add__ @suraj_o_p_musicbot __to your Group__ \n3) **Commands** : \n`/stream` (IN REPLY TO A VIDEO) \n`/stop`",   
+       await m.reply(f"**Hey, I'm a VC Video Player developed by Developer of** @SURAJ_O_P \n\n**To use it:-** __ \n1) Add this Bot to your Group and Make it Admin \n2) Add__ @suraj_o_p_musicbot __to your Group it is official bot __ \n3) **Commands** : \n`/stream` (IN REPLY TO A VIDEO) \n`/stop`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
-                                            "Support", url="t.me/@AXEL_SUPPORT")
+                                            "Support", url="https://t.me/AXEL_SUPPORT")
                                     ]]
                             ))
    else:
